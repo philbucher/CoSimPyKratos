e2c55d79bdd57bfccf75e954dc1af6b068abd8a7
philbucher <philipp.bucher@tum.de>
2020-09-03 09:56:16 +0200

    adding files

---
+++ pyKratos/IntervalUtility.py
@@ -0,0 +1,7 @@
+# TODO this should be implemented, see "kratos/utilities/interval_utility.h"
+class IntervalUtility(object):
+    def __init__(self, settings):
+        pass
+
+    def IsInInterval(self, current_time):
+        return True
